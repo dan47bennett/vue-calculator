0d19b4c5526ff51dd4b043b808017e7e926b7056
Dan Bennett <dghbennett47@gmail.com>
2020-04-21 15:32:25 +0100

    create display

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="calculator">
-    <div>Hello World</div>
+    <div class="display">{{ currentValue }}</div>
+    <button class="number">1</button>
+    <button class="number">2</button>
+    <button class="number">3</button>
+    <button class="number">4</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      currentValue: '123'
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .calculator {
+    display: grid;
     grid-template-columns: repeat(4, 1fr);
+  }
+
+  .display {
+    grid-column: 1 / 5;
+  }
+
+  .number {
+    background-color:darkgrey;
+    color: white
   }
 </style>
